3fc2e4297474ffde4a470c7b6e791a31fa039ba3
kemalmao19 <sebelumtidur14@gmail.com>
2023-11-27 17:04:08 +0700

    feat(auth): add icon for email and password

--- src/components/auth/components/Login.jsx
+++ src/components/auth/components/Login.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
 import { Button, Input } from "@nextui-org/react";
 import Link from 'next/link';
+import {MailIcon} from './Icons/MailIcon';
+import { PasswdIcon } from './Icons/PasswdIcon';
 
 export const Login = () => {
   return (
-    <div className='flex flex-col gap-3'>
-        <div className='text-center'>Welcome Back!</div>
-        <Input name="email" placeholder="email@domain.com" />
-        <Input name="password" placeholder="password" type="password" />
-        <Button color="primary">
-            Login
-        </Button>
-        <div className="flex gap-1">
-            <div>Don't have an account ?</div>
-            <Link href="#" className='text-blue-400'>Register</Link>
+    <div className='flex flex-col gap-5'>
+        <div className=''>
+          <div className='text-xl font-bold'>Welcome Back!</div>
+          <p className='text-sm'>Login to your account</p>
+        </div>
+        <div className='flex flex-col gap-3'>
+          <Input 
+            name="email" 
+              placeholder="email@domain.com" 
+              endContent={
+                <MailIcon />
+              }
+          />
+
+          <Input 
+            name="password" 
+            placeholder="password" 
+            type="password" 
+            endContent={
+              <PasswdIcon />
+            }
+          />
+
+          <Button color="primary">
+              Login
+          </Button>
+
+          <div className="flex gap-1 text-sm">
+              <div>Don't have an account ?</div>
+              <Link href="#" className='text-blue-400'>Register</Link>
+          </div>
         </div>
     </div>
   )
