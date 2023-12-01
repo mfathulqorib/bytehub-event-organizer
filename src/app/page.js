8f19e49ce5e6c1947fc01059c12a128c1c36824d
kemalmao19 <sebelumtidur14@gmail.com>
2023-12-01 16:47:10 +0700

    feat(homepage): fix hero add event section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { Header } from "@/components/homepage/components/Header";
 import { Hero } from "@/components/homepage/components/Hero";
+import { Events } from "@/components/homepage/components/Events";
 
 export default function Page() {
   return (
   <main>
     <Header/>
     <Hero/>
+    <Events />
   </main>
   )
 }
---
+++ src/components/homepage/components/Events.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Events = () => {
+  return (
+    <div>
+        <section>All Events Arround You</section>
+        <section>
+            Hehe
+        </section>
+    </div>
+  )
+}
