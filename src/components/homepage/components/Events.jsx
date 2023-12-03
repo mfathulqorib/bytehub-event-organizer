d5e90f7f93ef4c8a4bd106edb265fe708004ffc4
calmbing <mfathulqorib97@gmail.com>
2023-12-03 16:34:33 +0700

    update

--- src/components/homepage/components/Events.jsx
+++ src/components/homepage/components/Events.jsx
@@ -29,7 +29,6 @@ export const Events = ({ allData }) => {
   // };
 
   return (
-<<<<<<< HEAD
     <div className="flex flex-col gap-10 p-10">
       <section id="all-events" className="flex items-center justify-between">
         <h1 className="text-3xl font-extrabold">All Events Arround You</h1>
@@ -40,21 +39,11 @@ export const Events = ({ allData }) => {
             name="filtered-location"
             autoComplete="filtered-location"
             placeholder="Jakarta"
-=======
-    <div className='flex flex-col p-10 gap-10'>
-      <section id='all-events' className='flex justify-between items-center'>
-        <h1 className='text-3xl font-extrabold'>All Events Arround You</h1>
-        <div className='flex gap-3 items-center border-b-2 border-gray-500 py-3'>
-          <SearchIcon/>
-          <input type='text' placeholder='Jakarta' 
-            name='filter-by-city'
->>>>>>> refs/remotes/origin/8-homepage-list-of-all-events
             onChange={(event) => setSearchTerm(event.target.value)}
             className="px-3 focus:outline-none"
           />
         </div>
       </section>
-<<<<<<< HEAD
       <section className="grid max-h-[80vh] grid-cols-2 gap-5 overflow-y-auto p-4 shadow-lg shadow-black/30 lg:grid-cols-3">
         {filteredEvents.map(
           ({ name, description, location, date, isBanned, id }, index) => {
@@ -71,15 +60,6 @@ export const Events = ({ allData }) => {
             );
           },
         )}
-=======
-      <section className='grid grid-cols-2 lg:grid-cols-3 gap-5 max-h-screen  overflow-y-auto'>
-        {filteredEvents.map(({name, description, location, date, isBanned, id})=>{
-          return (
-            <Link href={`/events/${id}`} key={id}>
-              <EventCard name={name} description={description} location={location} date={date} isBanned={isBanned} />
-            </Link>)
-        })}
->>>>>>> refs/remotes/origin/8-homepage-list-of-all-events
       </section>
     </div>
   );
--- src/components/homepage/components/Footer.jsx
+++ src/components/homepage/components/Footer.jsx
@@ -8,8 +8,7 @@ export const Footer = () => {
     "https://images.unsplash.com/photo-1569930784237-ea65a2f40a83?q=80&w=1312&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
     <>
-<<<<<<< HEAD
-      <div className="flex items-center justify-center gap-10 p-20">
+      <div className="flex items-center justify-center gap-10 pb-20 pt-10">
         <Image
           src={fakeImg}
           width="0"
@@ -38,20 +37,6 @@ export const Footer = () => {
               <Button>Explore</Button>
             </Link>
           </div>
-=======
-        <div className='flex justify-center items-center p-20 gap-10'>
-            <Image src={fakeImg} width={300} height={700} sizes='100vh' className='h-auto lg:w-[30%]' alt=''/>
-            <div className='flex flex-col gap-5'>
-                <div className='flex flex-col'>
-                    <h1 className='text-3xl font-extrabold text-gray-800'>Join or Create Your Event</h1>
-                    <p className='text-xl text-gray-800'>Everything make easy</p>
-                </div>
-                <div className='flex flex-col gap-3'>
-                    <Link href={"/dashboard"}><Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white text-xl shadow-lg px-10 py-5" variant="bordered">Lets Create</Button></Link>
-                    <Link href={"#all-events"}><Button>Explore</Button></Link>
-                </div>
-            </div>
->>>>>>> refs/remotes/origin/8-homepage-list-of-all-events
         </div>
       </div>
       <div className="flex items-center justify-center bg-black p-3 text-orange-400">
