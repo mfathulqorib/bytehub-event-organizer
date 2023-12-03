81c0b5b106079c02f36cd2194682773d7c495b9c
calmbing <mfathulqorib97@gmail.com>
2023-12-03 22:06:22 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+"use client";
+
 import { Header } from "@/components/homepage/components/Header";
 import { Hero } from "@/components/homepage/components/Hero";
 import { Events } from "@/components/homepage/components/Events";
 import { Footer } from "@/components/homepage/components/Footer";
-import { API_URL } from "@/config/apiUrl";
 import { useFetchData } from "@/components/homepage/components/hooks/useFetchData";
 
 export default async function Page() {
