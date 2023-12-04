7bb07826ca384173894a2fa719325fbc9d1f346c
calmbing <mfathulqorib97@gmail.com>
2023-12-04 12:58:41 +0700

    add decode JWT

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar } from "@/components/Navbar";
 import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
+import { SECRET_KEY } from "@/config/apiUrl";
 
 export default function page() {
   // get cookie from browser
@@ -9,7 +10,7 @@ export default function page() {
   const token = cookieStore.get("token").value;
 
   // decode token
-  const payload = verify(token, process.env.NEXT_PUBLIC_SECRET_KEY);
+  const payload = verify(token, SECRET_KEY);
   console.log(payload);
   const { name } = payload;
 
--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,1 +1,2 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY;
